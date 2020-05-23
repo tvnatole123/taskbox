844d8bd7451eaf7f6a49bb0e4dd64050af43748e
Angular Developer <>
2020-05-23 18:36:07 -0400

    taskbox UI

---
+++ storybook-static/precache-manifest.8140022d0d27c0ccdce3d98320face70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ac864883d2a2a4e0c0b3e87fa21edc",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2d844c77b5aa3fd50dd7.bundle.js"
+  },
+  {
+    "url": "runtime~main.2d844c77b5aa3fd50dd7.bundle.js"
+  },
+  {
+    "revision": "509083d05f7803e305e9",
+    "url": "static/css/main.281f400c.chunk.css"
+  },
+  {
+    "url": "vendors~main.2d844c77b5aa3fd50dd7.bundle.js"
+  },
+  {
+    "url": "vendors~main.2d844c77b5aa3fd50dd7.bundle.js.LICENSE.txt"
+  }
+]);
