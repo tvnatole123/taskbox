fe34f667b3b85819d8d4179f6f08790fb263331c
nataliamalysh <natalia.malysheva@rangle.io>
2021-10-17 16:15:19 -0400

    screens, error state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { TaskModule } from './components/task.module';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { InboxScreenComponent } from './components/inbox-screen.component';
+import { PureInboxScreenComponent } from './components/pure-inbox-screen.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InboxScreenComponent, PureInboxScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pure-inbox-screen.stories.ts
@@ -0,0 +1,33 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+
+import { CommonModule } from '@angular/common';
+
+import { PureInboxScreenComponent } from './pure-inbox-screen.component';
+
+import { TaskModule } from './task.module';
+
+import { Store, NgxsModule } from '@ngxs/store';
+import { TasksState } from '../state/task.state';
+
+export default {
+  component: PureInboxScreenComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [PureInboxScreenComponent],
+      imports: [CommonModule, TaskModule, NgxsModule.forRoot([TasksState])],
+      providers: [Store],
+    }),
+  ],
+  title: 'PureInboxScreen',
+} as Meta;
+
+const Template: Story<PureInboxScreenComponent> = args => ({
+  props: args,
+});
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = {
+  error: true,
+};
